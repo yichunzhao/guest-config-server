8167fb794da7802729a3c5a76db9abfbbdae8ff3
yichunzhao <zhaoyichun@hotmail.com>
2020-07-19 16:09:22 +0200

    Initial commit

---
+++ src/test/java/com/ynz/democloud/configserver/GuestConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynz.democloud.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
